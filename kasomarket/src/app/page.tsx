028eb83e4f5d9c460d719a5a8ce3ad9e4072bc42
kawji <24.supavich@gmail.com>
2025-09-09 21:37:24 +0700

    Create page.tsx

---
+++ kasomarket/src/app/page.tsx
@@ -0,0 +1,14 @@
+import Topnav from "@/layout/navbar/topnav";
+import Navbar from "@/layout/navbar/navbar";
+
+
+
+
+export default function Home() {
+  return (
+    <div className="bg-[#dedede] w-full min-h-screen text-black ">
+      <Topnav />
+      <Navbar />
+    </div>
+  );
+}
