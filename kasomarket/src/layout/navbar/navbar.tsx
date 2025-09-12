931451445514f03354f7b6881f0ad92b9e5ae8c1
kawji <24.supavich@gmail.com>
2025-09-12 21:05:16 +0700

    Update navbar.tsx

--- kasomarket/src/layout/navbar/navbar.tsx
+++ kasomarket/src/layout/navbar/navbar.tsx
@@ -1,5 +1,5 @@
 import Logo from "@/ui/logo/logo"
-import { Search } from "lucide-react"
+import { Handbag, Search ,User} from "lucide-react"
 
 
 export default function Navbar() {
@@ -10,12 +10,18 @@ export default function Navbar() {
             </div>
             <div className=" w-[40vw]  relative h-full shrink-0 ">
                 <Search width={15} className="absolute text-black/35 left-5 top-1/2 translate-y-[-50%]  " />
-                <input type="text" placeholder=" ค้นหา" className="w-full py-2 px-15 text-black/75 outline-none placeholder:text-white/35 border border-black/35 rounded-full " />
+                <input type="text" placeholder="ค้นหาสินค้า" className="w-full py-2 px-15 text-black/75 outline-none placeholder:text-black/35 border border-black/19 rounded-full " />
             </div>
-            <div className="w-auto h-full bg-blue-500 ">
-                
+            <div className="w-auto h-full  items-center flex gap-5 ">
+                <span className=" flex justify-center items-center bg-black/5 rounded-full w-10.5 h-10.5 cursor-pointer  text-black/75 transition-colors hover:bg-black/12 " >
+                    <User width={20} />
+                    
+                </span>
+                <span className=" flex justify-center items-center bg-black/5 rounded-full w-10.5 h-10.5 cursor-pointer  text-black/65 transition-colors hover:bg-black/12 " >
+                    <Handbag width={20} />
+                    
+                </span>
             </div>
-
         </nav>
     )
 }
