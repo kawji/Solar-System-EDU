2f0bc0437fbc136b171ca7c56f40bd2c99cd4930
kawji <24.supavich@gmail.com>
2025-09-18 20:28:01 +0700

    Create navbar.tsx

---
+++ kasomarket/src/layout/feature/navbar.tsx
@@ -0,0 +1,14 @@
+import Topnav from "../navbar/topnav"
+import Mainnav from "../navbar/mainnav"
+import Footnav from "../navbar/footnav"
+
+export default function Navbar() {
+    return(
+    <>
+        <Topnav />  
+        <Mainnav />
+        <Footnav />
+    </>
+        
+    )
+}
