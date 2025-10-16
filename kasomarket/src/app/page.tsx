231e18d025c32e5250f20c49b1c81bba16387f61
kawji <24.supavich@gmail.com>
2025-10-16 20:12:02 +0700

    Update page.tsx

--- kasomarket/src/app/page.tsx
+++ kasomarket/src/app/page.tsx
@@ -4,9 +4,16 @@ import Carousel from "@/layout/feature/carousel";
 
 export default function Home() {
   return (
-    <div className="bg-[#dedede] w-full min-h-screen text-black ">
+    <div className="bg-[#ececec] w-full min-h-screen text-black ">
       <Navbar />
       <Carousel />
+      <div className=" w-150px h-auto  " >
+        <Carousel />
+      </div>
+
+
+
+
 
 
 
