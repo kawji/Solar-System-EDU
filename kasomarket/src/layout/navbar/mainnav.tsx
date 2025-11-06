41121cb429816615fe9e177697c75520673e5b47
kawji <24.supavich@gmail.com>
2025-11-06 21:18:26 +0700

    Update mainnav.tsx

--- kasomarket/src/layout/navbar/mainnav.tsx
+++ kasomarket/src/layout/navbar/mainnav.tsx
@@ -9,24 +9,15 @@ export default function Mainnav() {
     const { theme, toggleTheme } = useTheme();
 
     return(
-        <nav className="sticky w-full pt-5 pb-2 lg:pl-10 lg:pr-15 flex justify-between items-center  bg-white ">
-            <div className=" p-0 m-0  ">
-                <Logo size="lg" />     
-            </div>
-            <div className=" max-w-[40vw] w-full flex items-center justify-center  relative h-full shrink-0 ">
-                <Sectionsnav />
-            </div>
-            <div className="w-auto h-full  items-center flex gap-5 ">
-                <button className=" flex justify-center items-center bg-black/5 rounded-full w-10.5 h-10.5 cursor-pointer  text-black/75 transition-colors hover:bg-black/12 " 
-                  onClick={toggleTheme}
-                >
-                    <Sun  width={20} />
-                    
-                </button>
-                <span className=" flex justify-center items-center bg-black/5 rounded-full w-10.5 h-10.5 cursor-pointer  text-black/65 transition-colors hover:bg-black/12 " >
-                    <Github   width={20} />
-                    
-                </span>
+        <nav className="sticky w-full h-auto pt-5 pb-2  flex justify-center items-center  bg-white px-3 border-b border-b-black/7 shadow-2xs ">
+            <div className="flex items-center justify-between w-full h-full flex-1 max-w-[1200px]">
+                <div className=" p-0 m-0  ">
+                    <Logo size="lg" />     
+                </div>
+                <div className=" flex-1 w-full flex items-center justify-end  relative h-full shrink-0 ">
+                    <Sectionsnav />
+                </div>
+
             </div>
         </nav>
     )
@@ -39,4 +30,20 @@ export default function Mainnav() {
 // <div className=" w-[40vw]  relative h-full shrink-0 ">
 // <Search width={15} className="absolute text-black/35 left-5 top-1/2 translate-y-[-50%]  " />
 // <input type="text" placeholder="ค้นหาสินค้า" className="w-full py-2 px-15 text-black/75 outline-none placeholder:text-black/35 border border-black/19 rounded-full " />
+// </div>
+
+
+
+
+// <div className="w-auto h-full  items-center flex gap-5 ">
+// <button className=" flex justify-center items-center bg-black/5 rounded-full w-10.5 h-10.5 cursor-pointer  text-black/75 transition-colors hover:bg-black/12 " 
+//   onClick={toggleTheme}
+// >
+//     <Sun  width={20} />
+    
+// </button>
+// <span className=" flex justify-center items-center bg-black/5 rounded-full w-10.5 h-10.5 cursor-pointer  text-black/65 transition-colors hover:bg-black/12 " >
+//     <Github   width={20} />
+    
+// </span>
 // </div>
