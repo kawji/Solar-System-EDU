d3adc9ea50aff0904eeee662fe1d19d8c6d2f315
kawji <24.supavich@gmail.com>
2025-11-06 21:18:20 +0700

    Update topnav.tsx

--- kasomarket/src/layout/navbar/topnav.tsx
+++ kasomarket/src/layout/navbar/topnav.tsx
@@ -4,29 +4,30 @@ import { Mail } from 'lucide-react';
 
 export default function Topnav() {
     return(
-        <nav className="relative top-0 left-0 w-full py-2 lg:pl-10 lg:pr-20 flex justify-between  bg-black ">
-        <div className=" flex gap-5  flex-1  text-[13px] text-white/95">
+    <nav className="relative top-0 left-0 w-full py-2 flex justify-center  bg-black px-3 ">
+        <div className="flex items-center justify-between w-full h-full flex-1 max-w-[1200px]  ">
+            <div className=" flex gap-5  flex-1  text-[13px] text-white/95">
 
-            <span className="flex items-center gap-2 hover:text-blue-400 transition-colors cursor-pointer ">
-                <PhoneCall width={14} />
-                <p className=" font-extralight  ">08   680 4905</p>
-            </span>
+                <span className="flex items-center gap-2 hover:text-blue-400 transition-colors cursor-pointer ">
+                    <PhoneCall width={14} />
+                    <p className=" font-extralight  ">08   680 4905</p>
+                </span>
 
-            <span className="flex items-center gap-2 hover:text-blue-400 transition-colors cursor-pointer ">
-                <Mail width={14} />
-                <p className=" font-extralight t">std11038@tpu.ac.th</p>
-            </span>
-            <span className="flex items-center gap-2 hover:text-blue-400 transition-colors cursor-pointer ">
-                <p className=" font-extralight  ">อัปเดตเมื่อ 04/11/2025</p>
-            </span>
+                <span className="flex items-center gap-2 hover:text-blue-400 transition-colors cursor-pointer ">
+                    <Mail width={14} />
+                    <p className=" font-extralight t">std11038@tpu.ac.th</p>
+                </span>
+                <span className="flex items-center gap-2 hover:text-blue-400 transition-colors cursor-pointer ">
+                    <p className=" font-extralight  ">อัปเดตเมื่อ 04/11/2025</p>
+                </span>
 
+            </div>
+            <div className=" flex-1 flex items-center justify-end ">
+                <span className=" text-[13px] text-white/95 hover:text-blue-400 transition-colors cursor-pointer ">
+                    เนื้อหาที่ใกล้เคียง
+                </span>
+            </div>
         </div>
-        <div className=" flex-1 flex items-center justify-end ">
-            <span className=" text-[13px] text-white/95 hover:text-blue-400 transition-colors cursor-pointer ">
-                เนื้อหาที่ใกล้เคียง
-            </span>
-        </div>
-
     </nav>
     )
 }
