3d0d53d700738d0bce4dfae3c92046177fcd8c76
kawji <24.supavich@gmail.com>
2025-11-06 21:18:34 +0700

    Update navbar.tsx

--- kasomarket/src/layout/feature/navbar.tsx
+++ kasomarket/src/layout/feature/navbar.tsx
@@ -3,11 +3,11 @@ import Mainnav from "../navbar/mainnav"
 
 export default function Navbar() {
     return(
-    <>
+    <nav className="w-full h-auto ">
         <Topnav />  
         <Mainnav />
 
-    </>
+    </nav>
         
     )
 }
