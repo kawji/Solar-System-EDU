0aef346a421860e358c8b1d4a6631836a6387331
カウ <24.supavich@gmail.com>
2025-11-10 11:35:37 +0700

    Create tagcard.tsx

---
+++ kasomarket/src/layout/feature/tagcard.tsx
@@ -0,0 +1,29 @@
+
+import Tag from "@/app/components/tag"
+
+
+
+export default function TagCard() {
+
+    return(
+            <div className="w-full h-auto px-2 py-3 max-w-[900px] flex gap-1 border border-black/8 rounded flex-wrap items-center justify-start mt-10 ">
+        <span className="text-lg text-black/70 font-semibold shrink-0 ">
+            Tag ;
+        </span>
+    
+        <Tag text=" ระบบสุริยะ," />
+        <Tag text=" ระบบสุริยะ," />
+        <Tag text=" ระบบสุริยะ," />
+        <Tag text=" ระบบสุริยะ," />
+        <Tag text=" ระบบสุริยะ," />
+        <Tag text=" ระบบสุริยะ," />
+        <Tag text=" ระบบสุริยะ," />
+        <Tag text=" ระบบสุริยะ," />
+        <Tag text=" ระบบสุริยะ," />
+        <Tag text=" ระบบสุริยะ," />
+        <Tag text=" ระบบสุริยะ," />
+        <Tag text=" ระบบสุริยะ," />
+
+    </div>
+    )
+}
