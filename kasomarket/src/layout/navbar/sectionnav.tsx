84289221b34d358f0a784ddec622117596a74fb0
kawji <24.supavich@gmail.com>
2025-11-16 22:04:28 +0700

    Update sectionnav.tsx

--- kasomarket/src/layout/navbar/sectionnav.tsx
+++ kasomarket/src/layout/navbar/sectionnav.tsx
@@ -3,33 +3,16 @@ import clsx from "clsx"
 import { TextAlignEnd, X, Earth } from 'lucide-react';
 import { usePathname } from "next/navigation"
 import { useState } from "react";
+import { scrollToSection } from "@/utils/scrollToSections";
+import { SiGmail ,SiDiscord } from "react-icons/si";
+import { FaGithub ,FaFacebookF ,FaInstagram   } from "react-icons/fa";
 
 export default function Sectionsnav() {
     const path = usePathname();
-    const [isOpen, setIsOpen] = useState(false); // << state ควบคุม sidebar
+    const [isOpen, setIsOpen] = useState(false);
 
     return (
         <div className="flex items-center justify-end lg:text-[16px] gap-4 w-full h-auto ">
-
-            {/* Navbar (Desktop) */}
-            <span className={clsx("p-2 text-black/75 hover:text-blue-500 transition-colors cursor-pointer hidden ", path==='/' ? 'text-blue-700' : '' )}>
-                กำเนิดระบบสุริยะ
-            </span>
-            <span className="p-2 text-black/75 hover:text-blue-500 transition-colors cursor-pointer hidden ">
-                เขตของระบบสุริยะ
-            </span>
-            <span className="p-2 text-black/75 hover:text-blue-500 transition-colors cursor-pointer hidden ">
-                ส่วนประกอบระบบสุริยะ
-            </span>
-            <span className="p-2 text-black/75 hover:text-blue-500 transition-colors cursor-pointer hidden ">
-                ประเภทดาวเคราะห์
-            </span>
-            <span className="p-2 text-black/75 hover:text-blue-500 transition-colors cursor-pointer hidden ">
-                ดาวเคราะห์ทั้ง 8
-            </span>
-            <span className="p-2 text-black/75 hover:text-blue-500 transition-colors cursor-pointer hidden">
-                วัตถุท้องฟ้า
-            </span>
 
             <TextAlignEnd
                 width={50}
@@ -65,40 +48,100 @@ export default function Sectionsnav() {
                         หัวข้อเรื่อง
                     </div>
 
-                    <div className="w-full px-8 py-4 hover:bg-black/5 cursor-pointer">
+                    <div className="w-full px-8 py-4 hover:bg-black/5 cursor-pointer transition-colors"
+                    onClick={() => {
+                            scrollToSection('formation') 
+                            setIsOpen(false) 
+                        }}
+                    >
                         • การกำเนิดระบบสุริยะ
                     </div>
 
-                    <div className="w-full px-8 py-4 hover:bg-black/5 cursor-pointer">
+                    <div   className="w-full px-8 py-4 hover:bg-black/5 cursor-pointer transition-colors"
+                    onClick={() => {
+                        scrollToSection('zones')
+                        setIsOpen(false)
+                    }}
+                    >
                         • เขตต่างๆ ของระบบสุริยะ
                     </div>
 
-                    <div className="w-full px-8 py-4 hover:bg-black/5 cursor-pointer">
+                    <div className="w-full px-8 py-4 hover:bg-black/5 cursor-pointer transition-colors"
+                    onClick={() => {
+                        scrollToSection('components')
+                        setIsOpen(false)
+                    }}
+                    >
                         • ส่วนประกอบหลักของระบบสุริยะ
                     </div>
 
-                    <div className="w-full px-8 py-4 hover:bg-black/5 cursor-pointer">
+                    <div className="w-full px-8 py-4 hover:bg-black/5 cursor-pointer transition-colors"
+                    onClick={() => {
+                        scrollToSection('type')
+                        setIsOpen(false)
+                    }}
+                    >
                         • การแบ่งประเภทดาวเคราะห์
                     </div>
 
-                    <div className="w-full px-8 py-4 hover:bg-black/5 cursor-pointer">
+                    <div className="w-full px-8 py-4 hover:bg-black/5 cursor-pointer transition-colors"
+                    onClick={() => {
+                        scrollToSection('planet')
+                        setIsOpen(false)
+                    }}
+                    >
                         • ข้อมูลของดาวเคราะห์แต่ละดวง
                     </div>
 
-                    <div className="w-full px-8 py-4 hover:bg-black/5 cursor-pointer">
+                    <div  className="w-full px-8 py-4 hover:bg-black/5 cursor-pointer transition-colors "
+                    onClick={() => {
+                        scrollToSection('object')
+                        setIsOpen(false)
+                    }}
+                    >
                         • วัตถุท้องฟ้าอื่น ๆ
                     </div>
                 </div>
 
                 <div className="w-[85%] h-[1px] bg-black/18 my-5 mx-auto" />
 
-                <div className="w-full px-8 py-4 hover:bg-black/5 cursor-pointer">
+                <a href="https://mail.google.com" className="w-full px-8 py-4  hover:bg-black/5  text-black/95">
                     แจ้งปัญหาต่างๆ
-                </div>
-                <div className="w-full px-8 py-4 hover:bg-black/5 cursor-pointer">
+                </a>
+                <div className="w-full mt-4 px-8 py-4 text-black/95">
                     ติดต่อเรา & ช่องทางการติดต่อ
                 </div>
+                
+                <div className="w-full flex items-center justify-center pb-4 " >
+                    <div className="w-full h-auto flex px-8 ietms-center gap-4 ">
+                    <a href="https://mail.google.com" target="_blank" rel="noopener noreferrer" className="hover:text-red-600 transition-all p-3 border border-black/10 shadow-2xs hover:bg-black/3 duration-200 hover:scale-108 ">
+                        <SiGmail size={21} />
+                    </a>
+
+                    <a href="https://github.com" target="_blank" rel="noopener noreferrer" className="hover:text-gray-800 transition-all p-3 border border-black/10 shadow-2xs hover:bg-black/3 duration-200 hover:scale-108 ">
+                        <FaGithub size={21} />
+                    </a>
+
+                    <a href="https://discord.com" target="_blank" rel="noopener noreferrer" className="hover:text-indigo-500 transition-all p-3 border border-black/10 shadow-2xs hover:bg-black/3 duration-200 hover:scale-108 ">
+                        <SiDiscord size={21} />
+                    </a>
+
+                    <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer" className="hover:text-pink-500 transition-all p-3 border border-black/10 shadow-2xs hover:bg-black/3 duration-200 hover:scale-108 ">
+                        <FaInstagram size={21} />
+                    </a>
+
+                    <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer" className="hover:text-blue-600 transition-all p-3 border border-black/10 shadow-2xs hover:bg-black/3 duration-200 hover:scale-108 ">
+                        <FaFacebookF size={21} />
+                    </a>
+
+                    </div>
+
+
+                </div>
+            
+            
             </div>
+
         </div>
     );
 }
