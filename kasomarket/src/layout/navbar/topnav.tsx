fff1b496e1c090098d5344b2af58872cb64ea64b
kawji <24.supavich@gmail.com>
2025-11-16 22:04:26 +0700

    Update topnav.tsx

--- kasomarket/src/layout/navbar/topnav.tsx
+++ kasomarket/src/layout/navbar/topnav.tsx
@@ -6,25 +6,25 @@ export default function Topnav() {
     return(
     <nav className="relative top-0 left-0 w-full py-2 flex justify-center  bg-black px-3 ">
         <div className="flex items-center justify-between w-full h-full flex-1 max-w-[1200px]  ">
-            <div className=" flex gap-5  flex-1  text-[13px] text-white/95">
+            <div className=" flex gap-5  flex-1 flex-col sm:flex-row text-[13px] text-white/95">
 
-                <span className="flex items-center gap-2 hover:text-blue-400 transition-colors cursor-pointer ">
+                <span className="flex items-center gap-2 hover:text-blue-400 transition-colors cursor-pointer shrink-0">
                     <PhoneCall width={14} />
-                    <p className=" font-extralight  ">08   680 4905</p>
+                    <p className=" font-extralight  ">061 616 1610</p>
                 </span>
 
-                <span className="flex items-center gap-2 hover:text-blue-400 transition-colors cursor-pointer ">
+                <span className="flex items-center gap-2 hover:text-blue-400 transition-colors cursor-pointer shrink-0">
                     <Mail width={14} />
-                    <p className=" font-extralight t">std11038@tpu.ac.th</p>
+                    <p className=" font-extralight t">stdTPU6/1@tpu.ac.th</p>
                 </span>
-                <span className="flex items-center gap-2 hover:text-blue-400 transition-colors cursor-pointer ">
+                <span className="flex items-center gap-2 hover:text-blue-400 transition-colors cursor-pointer shrink-0">
                     <p className=" font-extralight  ">อัปเดตเมื่อ 04/11/2025</p>
                 </span>
 
             </div>
             <div className=" flex-1 flex items-center justify-end ">
                 <span className=" text-[13px] text-white/95 hover:text-blue-400 transition-colors cursor-pointer ">
-                    เนื้อหาที่ใกล้เคียง
+                    Solar System EDU
                 </span>
             </div>
         </div>
