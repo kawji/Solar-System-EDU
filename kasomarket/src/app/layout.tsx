43c60f92a432d687f87975fe3ea56d6f8d97447b
kawji <24.supavich@gmail.com>
2025-11-16 22:09:07 +0700

    Update layout.tsx

--- kasomarket/src/app/layout.tsx
+++ kasomarket/src/app/layout.tsx
@@ -1,18 +1,14 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Sarabun,  } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "./providers/ThemeProvider";
 
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+const sarabun = Sarabun({
+  variable: "--font-sarabun",
+  subsets: ["thai", "latin"],
+  weight: ["400", "700"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Solar System EDU — กำเนิดระบบสุริยะและการแบ่งเขตบริวารรอบดวงอาทิตย์",
@@ -25,10 +21,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    <html lang="th">
+      <body className={`${sarabun.variable} antialiased`}>
         <ThemeProvider>
           {children}
         </ThemeProvider>
